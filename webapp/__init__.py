55b55b523bb05b4a54b522fb06ccadad68d99dd7
Illuminater1 <romashcn@gmail.com>
2025-07-12 23:56:46 +0300

    Добавлены миграции, добавлена регистрация новых пользователей

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from webtrack.webapp.db import db
 from webtrack.webapp.user.models import User
 from webtrack.webapp.weather import weather_by_city
-from webtrack.webapp.config import SECRTET_KEY
+from webtrack.webapp.config import Config
 
 from webtrack.webapp.user.views import blueprint as user_blueprint
 from webtrack.webapp.admin.views import blueprint as admin_blueprint
@@ -13,10 +14,9 @@ from webtrack.webapp.news.views import blueprint as news_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile("config.py")
-    app.config['SECRET_KEY'] = SECRTET_KEY
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(Config)
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
@@ -25,6 +25,7 @@ def create_app():
     app.register_blueprint(user_blueprint)
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(news_blueprint)
+
 
     @login_manager.user_loader
     def load_user(user_id):
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for, Blueprint
 from flask_login import login_user, logout_user, current_user
 
-from webtrack.webapp.user.forms import LoginForm
+from webtrack.webapp.db import db
+from webtrack.webapp.user.forms import LoginForm, RegistrationForm
 from webtrack.webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/user')
@@ -38,3 +39,25 @@ def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = "Регистрация"
+    login_form = RegistrationForm()
+    return render_template('user/registration.html', page_title=title, form=login_form)
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, role='user')
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались')
+        return redirect(url_for('user.login'))
+
+    flash('Пожалуйста исправьте ошибки')
+    return redirect(url_for('user.registration'))
